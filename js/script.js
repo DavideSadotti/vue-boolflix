542c776397c40e7260a6090847557cd37a0036dd
Davide <d.sadotti@gmail.com>
2021-04-09 13:19:00 +0200

    Inizio Esercizio

---
+++ js/script.js
@@ -0,0 +1,17 @@
+var app = new Vue(
+  {
+    el: "#app",
+    data: {
+
+    },
+    methods: {
+
+    },
+    mounted: function(){
+      // axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+      //   .then((risposta) => {
+      //     this.cdList = risposta.data.response;
+      // });
+    }
+  }
+);
