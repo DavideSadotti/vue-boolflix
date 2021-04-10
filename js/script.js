4aaf1515f610edd707079b9bca3b1826c5c28d66
Davide <d.sadotti@gmail.com>
2021-04-10 12:16:43 +0200

    Milestone 2 Finita

--- js/script.js
+++ js/script.js
@@ -3,19 +3,19 @@ var app = new Vue(
     el: "#app",
     data: {
       filmTitle: "",
-      filmList: [],
+      filmList: []
     },
     methods: {
       search: function() {
-        axios.get('https://api.themoviedb.org/3/search/movie', {
+        axios.get('https://api.themoviedb.org/3/search/multi', {
           params: {
             api_key: "9e72b56abf296104112753fe36a0742f",
             query: this.filmTitle,
             language: "it-IT",
           }
         })
-        .then((return) => {
-          this.filmList = return.data.results;
+        .then((response) => {
+          this.filmList = response.data.results;
         })
         this.filmTitle = "";
       }
