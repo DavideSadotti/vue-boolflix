9617767d330dabf0c3bc536621e36f601f92f74b
Davide <d.sadotti@gmail.com>
2021-04-15 09:59:38 +0200

    fas e far star

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ var app = new Vue(
       filmList: []
     },
     methods: {
-      search: function() {
+      search: function(){
         axios.get('https://api.themoviedb.org/3/search/multi', {
           params: {
             api_key: "9e72b56abf296104112753fe36a0742f",
@@ -16,8 +16,21 @@ var app = new Vue(
         })
         .then((response) => {
           this.filmList = response.data.results;
+          this.voto();
         })
         this.filmTitle = "";
+      },
+      // NON FA
+      // voto: function(){
+      //   this.filmList.forEach((item) => {
+      //     var voti = (item.vote_average / 2).toFixed();
+      //     item.vote_average = voti;
+      //   });
+      // },
+      voto: function(item){
+        // return (item / 2).toFixed()
+        const newVote = (item / 2).toFixed();
+        return parseInt(newVote);
       }
     }
   }
